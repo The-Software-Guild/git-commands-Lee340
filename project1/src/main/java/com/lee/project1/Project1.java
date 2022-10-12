2448c5beffaac54876ba1d032c6088e77147982d
Boram Lee <boramlee340@gmail.com>
2022-10-12 08:59:17 +0100

    add project 1

---
+++ project1/src/main/java/com/lee/project1/Project1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.lee.project1;
+
+/**
+ *
+ * @author boram2
+ */
+public class Project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Boram!");
+    }
+}
