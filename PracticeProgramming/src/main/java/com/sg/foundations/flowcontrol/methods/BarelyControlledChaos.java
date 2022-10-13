860577f48f54d1ef19a938665520da15280ed7a0
Boram Lee <boramlee340@gmail.com>
2022-10-13 11:38:46 +0100

    Methods excercises

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/flowcontrol/methods/BarelyControlledChaos.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sg.foundations.flowcontrol.methods;
+
+/**
+ *
+ * @author boram2
+ */
+public class BarelyControlledChaos {
+    
+    public static void main(String[] args) {
+
+        String color = "magenta"; // call color method here
+        String animal = "mammoth"; // call animal method again here
+        String colorAgain = "red"; // call color method again here
+        int weight = 10; // call number method,
+            // with a range between 5 - 200
+        int distance = 15; // call number method,
+            // with a range between 10 - 20
+        int number = 15000; // call number method,
+            // with a range between 10000 - 20000
+        int time = 3; // call number method,
+            // with a range between 2 - 6            
+
+        System.out.println("Once, when I was very small...");
+
+        System.out.println("I was chased by a " + color + ", "
+            + weight + "lb " + " miniature " + animal
+            + " for over " + distance + " miles!!");
+
+        System.out.println("I had to hide in a field of over "
+            + number + " " + colorAgain + " poppies for nearly "
+            + time + " hours until it left me alone!");
+
+        System.out.println("\nIt was QUITE the experience, "
+            + "let me tell you!");
+    }
+
+    // ??? Method 1 ???
+    // ??? Method 2 ???
+    // ??? Method 3 ???
+}
